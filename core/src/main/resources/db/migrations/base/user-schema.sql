de6dc804f1834c16ad9c4b59bad9f909b0b66326
maksmolch <maksmolchdmitr@gmail.com>
2024-07-10 19:19:10 +0300

    init project

---
+++ core/src/main/resources/db/migrations/base/user-schema.sql
@@ -0,0 +1,6 @@
+create table user_table
+(
+    login varchar(32) not null primary key,
+    password varchar(32) not null,
+    full_name varchar(64) not null
+);
---
+++ core/src/test/java/maks/molch/dmitr/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package maks.molch.dmitr.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
