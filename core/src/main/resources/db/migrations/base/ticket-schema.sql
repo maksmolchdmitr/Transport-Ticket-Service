83112b205e3d7378ce6e0cb7870782e9e4c63abf
maksmolch <maksmolchdmitr@gmail.com>
2024-07-11 21:00:44 +0300

    delete user from ticket

--- core/src/main/resources/db/migrations/base/ticket-schema.sql
+++ core/src/main/resources/db/migrations/base/ticket-schema.sql
@@ -4,9 +4,6 @@ create table ticket_table
     date_and_time timestamp   not null,
     seat_number   int         not null,
     price         decimal     not null,
-    user_login    varchar(32) not null,
     foreign key (route_id) references route_table (id),
-    foreign key (user_login) references user_table (login),
-    constraint one_user_one_seat unique (user_login, seat_number),
     constraint unique_ticket unique (route_id, date_and_time, seat_number)
 );
