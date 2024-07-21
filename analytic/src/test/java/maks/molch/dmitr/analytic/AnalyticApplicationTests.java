17cad14372ac6b3885e88865bde5c32cf835fea3
maksmolchdmitr <maksmolchdmitr@gmail.com>
2024-07-21 15:29:34 +0300

    init analytic module

---
+++ analytic/src/test/java/maks/molch/dmitr/analytic/AnalyticApplicationTests.java
@@ -0,0 +1,13 @@
+package maks.molch.dmitr.analytic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyticApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
