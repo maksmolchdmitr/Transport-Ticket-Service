7b7bfe8078a46d010da0a5c26c87f75271f96c14
maksmolchdmitr <maksmolchdmitr@gmail.com>
2024-07-21 19:00:38 +0300

    fix kafka consumer

--- analytic/src/main/java/maks/molch/dmitr/analytic/config/KafkaConfig.java
+++ analytic/src/main/java/maks/molch/dmitr/analytic/config/KafkaConfig.java
@@ -1,14 +1,16 @@
 package maks.molch.dmitr.analytic.config;
 
 import maks.molch.dmitr.analytic.entity.TicketPurchase;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.LongSerializer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.LongDeserializer;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.support.serializer.JsonSerializer;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -17,17 +19,26 @@ import java.util.Map;
 public class KafkaConfig {
     @ConfigurationProperties(prefix = "kafka")
     public record KafkaProps(
-            String serverUrl
+            String serverUrl,
+            String topicName,
+            String groupId
     ) {
     }
 
     @Bean
-    public KafkaTemplate<Long, TicketPurchase> kafkaTemplate(KafkaProps kafkaProps) {
+    public KafkaListenerContainerFactory<?> kafkaListenerContainerFactory(KafkaProps kafkaProps) {
+        ConcurrentKafkaListenerContainerFactory<Long, TicketPurchase> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory(kafkaProps));
+        return factory;
+    }
+
+    public ConsumerFactory<Long, TicketPurchase> consumerFactory(KafkaProps kafkaProps) {
         Map<String, Object> properties = new HashMap<>();
-        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProps.serverUrl());
-        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class);
-        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-        DefaultKafkaProducerFactory<Long, TicketPurchase> kafkaProducer = new DefaultKafkaProducerFactory<>(properties);
-        return new KafkaTemplate<>(kafkaProducer);
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProps.serverUrl());
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, kafkaProps.groupId());
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, LongDeserializer.class);
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        return new DefaultKafkaConsumerFactory<>(properties);
     }
 }
